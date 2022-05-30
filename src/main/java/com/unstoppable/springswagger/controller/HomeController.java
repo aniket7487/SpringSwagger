f5c334bc430d299e09cc13d366aca96f6e289dbd
Aniket Mane <aniket.mane84@gmail.com>
2022-05-30 12:02:45 +0530

    Commit 2

--- src/main/java/com/unstoppable/springswagger/controller/HomeController.java
+++ src/main/java/com/unstoppable/springswagger/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	@GetMapping("home")
-	public String getHome() {
-		return "SUCCESS";
+	public String getHome(String str) {
+		return "SUCCESS2";
 	}
 }
